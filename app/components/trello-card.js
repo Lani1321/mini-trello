4a5ca1d31e0ccef86f029988b0833c8a691849db
Lani1321 <lani.berry13@gmail.com>
2018-02-11 22:26:44 -0500

    set property to true to open modal

--- app/components/trello-card.js
+++ app/components/trello-card.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   actions: {
     deleteCard: function(card) {
       this.list.cards.removeObject(card);
+    },
+    openModal: function() {
+      this.set('enabled', true);
     }
   }
 });
