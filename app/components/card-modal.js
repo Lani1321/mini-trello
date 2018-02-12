87642e2dc6fb961c3d191e0d9416379b3f0d5cc1
Lani1321 <lani.berry13@gmail.com>
2018-02-11 23:16:19 -0500

    add action to delete card and confirm delete

--- app/components/card-modal.js
+++ app/components/card-modal.js
@@ -4,6 +4,12 @@ export default Ember.Component.extend({
   actions: {
     closeModal: function(){
       this.set('enabled', false);
-    }
+    },
+    deleteCard: function(card) {
+      if (confirm("Are you sure you want to delete this card?")) {
+        this.list.cards.removeObject(card);
+      }
+      this.set('enabled', false);
+    },
   }
 });
