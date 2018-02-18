872ed1dcee7c97adc1da231593df83b3ef64f510
Lani1321 <lani.berry13@gmail.com>
2018-02-18 11:48:44 -0500

    add comments

--- app/components/create-card.js
+++ app/components/create-card.js
@@ -3,8 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     createCard() {
+      // Ember is able to pull the value based on the action
+      // New card value is being pulled from template
       var cardName = this.get("newCard")
-
+      
+      // Seeing if it's false or empty and returning it if it is then 
+      // itll never make it to addobject
       if(!cardName){return false;}
       if(!cardName.trim()) {return;}
 
